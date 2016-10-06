0a3d43526439c906f5ce65bf8ba08211dccd9b98
dongtiandemeng <1144310819@qq.com>
2016-10-06 16:47:10 +0800

    add mypage

--- app/pages/contact/contact.ts
+++ app/pages/contact/contact.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ModalController} from 'ionic-angular';
+import {ViewController} from 'ionic-angular';
+import {Login} from '../login/login';
+
 
 @Component({
   templateUrl: 'build/pages/contact/contact.html'
 })
 export class ContactPage {
-  constructor(public navCtrl: NavController) {
+  constructor(public modalCtrl: ModalController,public nav:NavController) {    
+  }
+
+ presentModal() {
+    // let modal = this.modalCtrl.create(Login);
+    // modal.present();
+    this.nav.push(Login);
   }
 }
+
+// @Component({
+//   templateUrl: 'modal-content.html'
+// })
+// export class ModalsContentPage {
+//   character;
+
+//   constructor(
+//       public viewCtrl: ViewController
+//   ) {
+
+//   }
+
+//   dismiss() {
+//     this.viewCtrl.dismiss();
+//   }
+// }
